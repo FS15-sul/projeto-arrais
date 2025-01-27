dc1e73324fee8171721c120c8cf3bcdf048ae31e
Francisco Gleidson Teixeira Rocha <Francisco.Rocha@digitalcollege.internal>
2025-01-27 12:06:19 -0300

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,28 @@
 import { Button } from "antd";
+import { Table } from "antd";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+
+    const [dados, setDados] = useState([]);
+
+    function buscarDados(){
+        fetch("https://projeto-arrais-api.onrender.com/aquisicoes")
+        .then(res => res.json())
+        .then(res => {
+            setDados(res);
+        })
+    }
+
+    useEffect(() => {
+        buscarDados();
+    },[])
+
     return (
         <>
             <div className="w-full h-[50px] flex justify-end items-center">
                 <Button
-                    type="primary" 
+                    type="primary"
                     size="large"
                     className="bg-laranja hover:!bg-laranja2"
                 >
@@ -14,11 +31,45 @@ const Home = () => {
             </div>
             <section>
                 <h1 className="text-2xl font-bold mb-4">Tabela de veículos</h1>
-                
+                <Table
+                    dataSource={dados}
+                >
+                    {/* <Table.Column
+                        key={"CD_AQUISICAO"}
+                        title={"ID"}
+                        dataIndex={"CD_AQUISICAO"}
+                        className="w-[50px]"
+                    /> */}
+                    <Table.Column
+                        key={"NR_PLACA"}
+                        title={"Placa"}
+                        dataIndex={"NR_PLACA"}
+                    />
+                    <Table.Column
+                        key={"NM_COR"}
+                        title={"Cor"}
+                        dataIndex={"NM_COR"}
+                    />
+                    <Table.Column
+                        key={"NM_ANO"}
+                        title={"Ano"}
+                        dataIndex={"NM_ANO"}
+                    />
+                    <Table.Column
+                        key={"NM_MODELO"}
+                        title={"Modelo"}
+                        dataIndex={"NM_MODELO"}
+                    />
+                    <Table.Column
+                        key={"VL_VEICULO"}
+                        title={"Preço"}
+                        dataIndex={"VL_VEICULO"}
+                    />
+                </Table>
             </section>
         </>
-        
+
     );
 }
- 
+
 export default Home;
